434cc8b2604be396e6a3693158df7b6734124d67
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-04-12 22:21:51 -0300

    Criação da Classe CargoController

--- src/main/java/com/rubenialima/web/controller/CargoController.java
+++ src/main/java/com/rubenialima/web/controller/CargoController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/funcionarios")
-public class FuncionarioController {
+@RequestMapping("/cargos")
+public class CargoController {
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
-		return "/funcionario/cadastro";
+		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
-		return"/funcionario/lista";
+		return"/cargo/lista";
 	}
 
 }
