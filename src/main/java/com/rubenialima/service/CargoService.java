b51cb152f26b76ae665a73148647ed36408fb649
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-04-17 22:52:20 -0300

    Incluindo os Services

---
+++ src/main/java/com/rubenialima/service/CargoService.java
@@ -0,0 +1,18 @@
+package com.rubenialima.service;
+
+import java.util.List;
+
+import com.rubenialima.domain.Cargo;
+
+public interface CargoService {
+
+	void salvar(Cargo cargo);
+	
+	void editar(Cargo cargo);
+	
+	void excluir(Long id);
+	
+	Cargo buscarPorId(Long id);
+	
+	List<Cargo> buscarTodos();
+}
---
+++ src/main/java/com/rubenialima/service/DepartamentoService.java
@@ -0,0 +1,5 @@
+package com.rubenialima.service;
+
+public interface DepartamentoService {
+
+}
