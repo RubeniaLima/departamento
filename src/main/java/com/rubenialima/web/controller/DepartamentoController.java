355e919ef56648472c325b436df24c726850e105
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-04-24 21:35:02 -0300

    Inserindo departamentos

--- src/main/java/com/rubenialima/web/controller/DepartamentoController.java
+++ src/main/java/com/rubenialima/web/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.rubenialima.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rubenialima.domain.Departamento;
+import com.rubenialima.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
@@ -17,5 +25,12 @@ public class DepartamentoController {
 	public String listar() {
 		return"/departamento/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
 
 }
