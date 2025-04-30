e8699709091c7fbd472d89efbe1f1ecaff3e2cad
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-04-29 21:29:21 -0300

    Inclusão do metodo preEditar  em departamentos

--- src/main/java/com/rubenialima/web/controller/DepartamentoController.java
+++ src/main/java/com/rubenialima/web/controller/DepartamentoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,4 +36,9 @@ public class DepartamentoController {
 	}
 	
 
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long  id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "/departamento/cadastro";
+	}
 }
