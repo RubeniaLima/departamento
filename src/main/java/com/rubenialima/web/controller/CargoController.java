88da31cd7d3dd85709df96816d61d59efec4f12f
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-05-06 22:28:04 -0300

    Implementado o metodo editar Cargos

--- src/main/java/com/rubenialima/web/controller/CargoController.java
+++ src/main/java/com/rubenialima/web/controller/CargoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,16 +15,23 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.rubenialima.domain.Cargo;
 import com.rubenialima.domain.Departamento;
 import com.rubenialima.service.CargoService;
+import com.rubenialima.service.CargoServiceImpl;
 import com.rubenialima.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
+
+    private final CargoServiceImpl cargoServiceImpl;
 	
 	@Autowired
 	private CargoService cargoService;
 	@Autowired
 	private DepartamentoService departamentoService;
+
+    CargoController(CargoServiceImpl cargoServiceImpl) {
+        this.cargoServiceImpl = cargoServiceImpl;
+    }
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar( Cargo cargo) {
@@ -40,6 +48,19 @@ public class CargoController {
 	public String salvar(Cargo cargo, RedirectAttributes attr) {
 		cargoService.salvar(cargo);
 		attr.addFlashAttribute("success", "Cargo inserido com sucesso");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Registro atualizado com sucesso.");
 		return "redirect:/cargos/cadastrar";
 	}
 	
