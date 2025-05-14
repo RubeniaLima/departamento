23f3f40b7975e6e7d6ebef329499e9a410ed65b4
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-05-13 22:06:04 -0300

    Inserindo o metodo salvar na classe FuncionarioController

--- src/main/java/com/rubenialima/web/controller/FuncionarioController.java
+++ src/main/java/com/rubenialima/web/controller/FuncionarioController.java
@@ -1,12 +1,21 @@
 package com.rubenialima.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.rubenialima.domain.Funcionario;
+import com.rubenialima.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
@@ -16,6 +25,13 @@ public class FuncionarioController {
 	@GetMapping("/listar")
 	public String listar() {
 		return"/funcionario/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
 	}
 
 }
