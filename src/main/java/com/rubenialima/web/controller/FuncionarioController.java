e652607aab72e338e3e289cb317e0ebe557b4bee
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-05-18 22:16:14 -0300

    Criação do metodo preEditar na classe FuncionarioController

--- src/main/java/com/rubenialima/web/controller/FuncionarioController.java
+++ src/main/java/com/rubenialima/web/controller/FuncionarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,6 +44,12 @@ public class FuncionarioController {
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
 		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.buscarPorId(id));
+		return "funcionario/cadastro";
 	}
 	
 	@ModelAttribute("cargos")
