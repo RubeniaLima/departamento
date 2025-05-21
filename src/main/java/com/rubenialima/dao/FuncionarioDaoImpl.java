58f6193094d1101da049ca9f0f10e27117ff3e0c
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-05-20 22:01:28 -0300

    Incluindo o metodo getPorNome na classe FuncionarioController

--- src/main/java/com/rubenialima/dao/FuncionarioDaoImpl.java
+++ src/main/java/com/rubenialima/dao/FuncionarioDaoImpl.java
@@ -1,10 +1,21 @@
 package com.rubenialima.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.rubenialima.domain.Funcionario;
 
+import jakarta.persistence.TypedQuery;
+
 @Repository
 public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implements FuncionarioDao{
 
+	
+	public List<Funcionario> findByNome(String nome) {
+		
+				return createQuery("select f from Funcionario f where f.nome like concat('%', ?1, '%')", nome);
+
+	}
+	
 }
