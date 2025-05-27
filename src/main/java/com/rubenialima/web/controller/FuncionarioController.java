d1147d7545509c9ed3b34408f23ff734054a0570
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-05-26 22:51:33 -0300

    Incluindo as validacoes nas classes Funcionario e Endereco

--- src/main/java/com/rubenialima/web/controller/FuncionarioController.java
+++ src/main/java/com/rubenialima/web/controller/FuncionarioController.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,8 @@ import com.rubenialima.domain.Funcionario;
 import com.rubenialima.domain.UF;
 import com.rubenialima.service.CargoService;
 import com.rubenialima.service.FuncionarioService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/funcionarios")
@@ -44,7 +47,11 @@ public class FuncionarioController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+	public String salvar(@Valid Funcionario funcionario,BindingResult result  ,RedirectAttributes attr) {
+		
+		if(result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
 		return "redirect:/funcionarios/cadastrar";
@@ -59,7 +66,11 @@ public class FuncionarioController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+	public String editar(@Valid Funcionario funcionario, BindingResult result,RedirectAttributes attr) {
+		
+		if(result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
 		funcionarioService.editar(funcionario);
 		attr.addAttribute("success", "Funcionario editado com sucesso.");
 		return "redirect:/funcionarios/listar";
